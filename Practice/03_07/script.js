511bfe3de546470cd96474f1e1898622a8d07a2f
Myra James <Treesmellgood@gmail.com>
2021-05-03 12:11:28 -0400

    i made an object

--- Practice/03_07/script.js
+++ Practice/03_07/script.js
@@ -7,3 +7,11 @@
  * - Find an object that has another object inside of it to create a nested object.
  * - Test your objects in the browser console by accessing the entire object and its specific properties.
  */
+
+
+var book = {
+    title: "Bible",
+    type: "Religious",
+    size: "Large",
+    translation: "KJV"
+};
