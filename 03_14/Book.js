382138fcc1e9acc5933f974f703e47da38a49177
Myra James <Treesmellgood@gmail.com>
2021-05-05 19:39:21 -0400

    Book classes

---
+++ 03_14/Book.js
@@ -0,0 +1,17 @@
+class Book {
+    constructor(
+        title,
+        author,
+        pageNum,
+    ){
+    this.title = title;
+    this.author = author;
+    this.pageNum = pageNum;
+}
+newBook(title, author, pageNum){
+    this.title = title;
+    this.author = author;
+    this.pageNum = pageNum;
+}};
+
+export default Book;
